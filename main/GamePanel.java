e8f71334c0abfba653aa483e8dbd15cb6666b90d
Erceanu <erceanu.vlad@gmail.com>
2024-10-06 16:18:51 +0200

    Added graphics2D and drew a rectangle right in the middle of JFrame

--- main/GamePanel.java
+++ main/GamePanel.java
@@ -2,9 +2,12 @@ package main;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel{
+public class GamePanel extends JPanel implements Runnable{
     
     //Settings for the screen ()
 
@@ -22,10 +25,46 @@ public class GamePanel extends JPanel{
     final int screenHeight = tileSize * maxScreenRow;
     final int screenWidth = tileSize * maxScreenCol;
 
+    Thread gameThread;
+
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.darkGray);
         this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+    
+    public void run() {
+
+        while(gameThread != null) {
+
+//            System.out.println("The game loop is running");
+
+            // 1 update information such as the character positions
+            Update();
+            // 2 draw the screen with the updated information
+            repaint();
+        }
+    }
+
+    public void Update() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+        
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+
+        g2.setColor(Color.white);
+
+        g2.fillRect(screenWidth/2-tileSize/2, screenHeight/2-tileSize/2, tileSize, tileSize);
+
+        g2.dispose();
     }
 
 }
