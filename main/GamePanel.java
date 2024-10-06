5f607e5cc1d0ff1066513d2020f83b802d76fecf
Erceanu <erceanu.vlad@gmail.com>
2024-10-06 17:48:47 +0200

    KeyHandler and game time (using the delta method), might need to look into the future for how to make the player character move inmore directions at the same time, and do it at the same speed

--- main/GamePanel.java
+++ main/GamePanel.java
@@ -25,12 +25,22 @@ public class GamePanel extends JPanel implements Runnable{
     final int screenHeight = tileSize * maxScreenRow;
     final int screenWidth = tileSize * maxScreenCol;
 
+    int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+
+    //Set player's default position
+    int playerX = (screenWidth - tileSize)/2;
+    int playerY = (screenHeight - tileSize)/2;
+    int playerSpeed = 4;
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.darkGray);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread() {
@@ -40,19 +50,42 @@ public class GamePanel extends JPanel implements Runnable{
     
     public void run() {
 
+        double drawInterval = 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        
+
         while(gameThread != null) {
 
-//            System.out.println("The game loop is running");
+            currentTime = System.nanoTime();
 
-            // 1 update information such as the character positions
-            Update();
-            // 2 draw the screen with the updated information
-            repaint();
+            delta += (currentTime - lastTime) / drawInterval;
+
+            lastTime = currentTime;
+
+            if(delta >= 1) {
+                update();
+                repaint();
+                delta--;
+            }
+
         }
     }
 
-    public void Update() {
-
+    public void update() {
+        if(keyH.upPressed == true) {
+            playerY -= playerSpeed;
+        }
+        else if(keyH.downPressed == true) {
+            playerY += playerSpeed;
+        }
+        else if(keyH.leftPressed == true) {
+            playerX -= playerSpeed;
+        }
+        else if(keyH.rightPressed == true) {
+            playerX += playerSpeed;
+        }
     }
 
     public void paintComponent(Graphics g) {
@@ -62,7 +95,7 @@ public class GamePanel extends JPanel implements Runnable{
 
         g2.setColor(Color.white);
 
-        g2.fillRect(screenWidth/2-tileSize/2, screenHeight/2-tileSize/2, tileSize, tileSize);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
 
         g2.dispose();
     }
