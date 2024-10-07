979ee34d4e9973494ce9211188a5cfe73ef45389
Erceanu <erceanu.vlad@gmail.com>
2024-10-07 13:21:02 +0200

    After a long night an some hours in the morning I finally did it, I managed to make our movement dependant on vectors. This way when we move diagonally our player character moves as fast as in the horizontal and vertical directions. The vectors are going to be of good use to other things too

--- main/KeyHandler.java
+++ main/KeyHandler.java
@@ -5,7 +5,7 @@ import java.awt.event.KeyListener;
 
 public class KeyHandler implements KeyListener{
 
-    public boolean upPressed, downPressed, leftPressed, rightPressed;
+    public boolean upPressed, downPressed, rightPressed, leftPressed;
 
     @Override
     public void keyTyped(KeyEvent e) {
@@ -13,17 +13,16 @@ public class KeyHandler implements KeyListener{
 
     @Override
     public void keyPressed(KeyEvent e) {
-        
         int code = e.getKeyCode();
 
         if(code == KeyEvent.VK_W) {
             upPressed = true;
         }
-        if(code == KeyEvent.VK_A) {
-            leftPressed = true;
-        }
         if(code == KeyEvent.VK_S) {
             downPressed = true;
+        }
+        if(code == KeyEvent.VK_A) {
+            leftPressed = true;
         }
         if(code == KeyEvent.VK_D) {
             rightPressed = true;
@@ -32,17 +31,16 @@ public class KeyHandler implements KeyListener{
 
     @Override
     public void keyReleased(KeyEvent e) {
-        
         int code = e.getKeyCode();
-
+        
         if(code == KeyEvent.VK_W) {
             upPressed = false;
         }
-        if(code == KeyEvent.VK_A) {
-            leftPressed = false;
-        }
         if(code == KeyEvent.VK_S) {
             downPressed = false;
+        }
+        if(code == KeyEvent.VK_A) {
+            leftPressed = false;
         }
         if(code == KeyEvent.VK_D) {
             rightPressed = false;
